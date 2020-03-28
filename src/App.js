4af0b35cff175c828194b6d16ae701c887b6090a
satz-github-project <62586183+satz-github-project@users.noreply.github.com>
2020-03-28 15:14:57 +0530

    Complete Project Basic things commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {CardList} from './components/card-list/card-list.component';
-
+import {SearchBox} from './components/search-box/search-box.component';
 
 import './App.css';
 
@@ -9,8 +9,10 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField:''
     };
+
   }
 
   componentDidMount(){
@@ -19,10 +21,19 @@ class App extends Component {
     .then(users => this.setState({monsters:users}));
   }
 
+  handleChange =e =>{
+    this.setState({searchField:e.target.value})
+  }
+
   render() {
+
+    const {monsters,searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return ( 
-      <div className = "App" > 
-      <CardList monsters = {this.state.monsters}>
+      <div className = "App" >      
+      <h1>Monster Rolodex</h1> 
+      <SearchBox placeHolder='search monster' handleChange={this.handleChange}></SearchBox>
+      <CardList monsters = {filteredMonsters}>
         </CardList>
       
       </div>
