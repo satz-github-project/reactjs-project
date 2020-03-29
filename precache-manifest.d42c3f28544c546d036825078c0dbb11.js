aa00ab8912f1f59407d0baa5bbfe2f5afa0eba63
satz-github-project <62586183+satz-github-project@users.noreply.github.com>
2020-03-29 11:10:12 +0530

    Updates

---
+++ precache-manifest.d42c3f28544c546d036825078c0dbb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006683ce93ed45ba2f0abc534d7824d9",
+    "url": "/reactjs-project/index.html"
+  },
+  {
+    "revision": "29dcd516d27838ecccfe",
+    "url": "/reactjs-project/static/css/main.242f5969.chunk.css"
+  },
+  {
+    "revision": "842e221c1d5789f1fb86",
+    "url": "/reactjs-project/static/js/2.e7d73f8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-project/static/js/2.e7d73f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29dcd516d27838ecccfe",
+    "url": "/reactjs-project/static/js/main.de2f6e0d.chunk.js"
+  },
+  {
+    "revision": "582cdf6be5804faa7874",
+    "url": "/reactjs-project/static/js/runtime-main.3cddf86b.js"
+  }
+]);
